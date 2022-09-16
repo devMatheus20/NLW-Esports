dedb1f66d99c60fbc9bffc3cd3b97e9c28c6b749
Matheus Santos <matheussantos77@outlook.com>
2022-09-15 21:21:15 -0300

    feat: home page structure

---
+++ Mobile/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
+}
